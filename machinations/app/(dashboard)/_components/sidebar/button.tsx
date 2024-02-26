77a3beac2582015ceb629d18474529486f7c7a3a
Lera Rakhmankulova <lerakhmankulova@mail.ru>
2024-02-26 08:02:41 +0300

    org customizing

--- machinations/app/(dashboard)/_components/sidebar/button.tsx
+++ machinations/app/(dashboard)/_components/sidebar/button.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Hint } from "@/components/hint";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { CreateOrganization } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
@@ -8,12 +9,19 @@ export const Button = () => {
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
-          <button
-            className="bg-white/25 h-full w-full rounded-md
-           flex items-center justify-center opacity-60 hover:opacity-100 transition"
+          <Hint
+            label="Create organization"
+            side="right"
+            align="start"
+            sideOffset={18}
           >
-            <Plus className="text-white" />
-          </button>
+            <button
+              className="bg-white/25 h-full w-full rounded-md
+           flex items-center justify-center opacity-60 hover:opacity-100 transition"
+            >
+              <Plus className="text-white" />
+            </button>
+          </Hint>
         </div>
       </DialogTrigger>
       <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
