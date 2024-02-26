5c1b38551b0f1de966edbef9ce80909f9061e5a4
Lera Rakhmankulova <lerakhmankulova@mail.ru>
2024-02-26 11:11:30 +0300

    start interface

--- machinations/app/(dashboard)/page.tsx
+++ machinations/app/(dashboard)/page.tsx
@@ -1,16 +1,14 @@
 import Image from "next/image";
 import RootLayout from "./../layout";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useOrganization } from "@clerk/nextjs";
+import { EmptyOrganization } from "./_components/empty-org";
 
 const DashboardPage = () => {
   return (
-    <div className="flex flex-col gap-y-4">
-      <div>This is an auth page</div>
-      <div>
-        <UserButton />
-      </div>
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      <EmptyOrganization />
     </div>
   );
-}
+};
 
 export default DashboardPage;
