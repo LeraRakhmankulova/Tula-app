24c67fb66c13888865d914e636c1039e9265570f
Lera Rakhmankulova <lerakhmankulova@mail.ru>
2024-02-29 13:39:36 +0300

    updt

---
+++ machinations/app/board/[boardId]/_components/toolbar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export const Toolbar = () => {
+  return (
+    <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
+      <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
+        <div>Pencil</div>
+        <div>Pencil 2</div>
+        <div>Pencil 3</div>
+        <div>Pencil 4</div>
+      </div>
+      <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
+        <div>Undo</div>
+        <div>Redo</div>
+      </div>
+    </div>
+  );
+};
