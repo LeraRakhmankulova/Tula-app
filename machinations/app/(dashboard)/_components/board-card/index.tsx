891283eb0e426aacf2bb63c971f7375172248c7f
Lera Rakhmankulova <lerakhmankulova@mail.ru>
2024-03-01 10:40:23 +0300

    delete actions

--- machinations/app/(dashboard)/_components/board-card/index.tsx
+++ machinations/app/(dashboard)/_components/board-card/index.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import { useAuth } from "@clerk/nextjs";
 import { MoreHorizontal } from "lucide-react";
 import { Footer } from "./footer";
+import { Actions } from "@/components/actions";
 
 interface BoardCardProps {
   id: string;
@@ -18,7 +19,7 @@ interface BoardCardProps {
   imageUrl: string;
   orgId: string;
   isFavorite: boolean;
-};
+}
 
 export const BoardCard = ({
   id,
@@ -37,49 +38,35 @@ export const BoardCard = ({
     addSuffix: true,
   });
 
-//   const {
-//     mutate: onFavorite,
-//     pending: pendingFavorite,
-//   } = useApiMutation(api.board.favorite);
-//   const {
-//     mutate: onUnfavorite,
-//     pending: pendingUnfavorite,
-//   } = useApiMutation(api.board.unfavorite);
+  //   const {
+  //     mutate: onFavorite,
+  //     pending: pendingFavorite,
+  //   } = useApiMutation(api.board.favorite);
+  //   const {
+  //     mutate: onUnfavorite,
+  //     pending: pendingUnfavorite,
+  //   } = useApiMutation(api.board.unfavorite);
 
-//   const toggleFavorite = () => {
-//     if (isFavorite) {
-//       onUnfavorite({ id })
-//         .catch(() => toast.error("Failed to unfavorite"))
-//     } else {
-//       onFavorite({ id, orgId })
-//         .catch(() => toast.error("Failed to favorite"))
-//     }
-//   };
+  //   const toggleFavorite = () => {
+  //     if (isFavorite) {
+  //       onUnfavorite({ id })
+  //         .catch(() => toast.error("Failed to unfavorite"))
+  //     } else {
+  //       onFavorite({ id, orgId })
+  //         .catch(() => toast.error("Failed to favorite"))
+  //     }
+  //   };
 
   return (
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/130] border rounded-lg flex flex-col justify-between overflow-hidden">
         <div className="relative flex-1 bg-amber-50">
-          <Image
-            src={imageUrl}
-            alt={title}
-            fill
-            className="object-fit"
-          />
-          {/* 
-          <Actions
-            id={id}
-            title={title}
-            side="right"
-          > */}
-            <button
-              className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none"
-            >
-              <MoreHorizontal
-                className="text-white opacity-75 hover:opacity-100 transition-opacity"
-              />
+          <Image src={imageUrl} alt={title} fill className="object-fit" />
+          <Actions id={id} title={title} side="right">
+            <button className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none">
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity" />
             </button>
-          {/* </Actions> */}
+          </Actions>
         </div>
         <Footer
           isFavorite={isFavorite}
--- machinations/app/(dashboard)/_components/navbar.tsx
+++ machinations/app/(dashboard)/_components/navbar.tsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
   const { organization } = useOrganization();
 
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+    <div className="flex items-center gap-x-4 p-5 bg-white">
       <div className="hidden lg:flex lg:flex-1">
         <SearchInput />
       </div>
