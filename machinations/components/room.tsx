bd92dae34ad48f305e3b04a1fbb7e223d1856ea4
Lera Rakhmankulova <lerakhmankulova@mail.ru>
2024-03-01 07:21:05 +0300

    start with

---
+++ machinations/components/room.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { ClientSideSuspense } from "@liveblocks/react";
+import { LiveMap, LiveList, LiveObject } from "@liveblocks/client";
+
+import { RoomProvider } from "@/liveblocks.config";
+
+interface RoomProps {
+  children: React.ReactNode;
+  roomId: string;
+//   fallback: NonNullable<React.ReactNode> | null;
+}
+
+export const Room = ({ children, roomId,  }: RoomProps) => {
+  return (
+    <RoomProvider
+      id={roomId}
+      initialPresence={{
+        cursor: null,
+        selection: [],
+        pencilDraft: null,
+        penColor: null,
+      }}
+    >
+      <ClientSideSuspense fallback={<div>Loading</div>}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
