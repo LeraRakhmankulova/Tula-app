c3f24b50353306ea7e4cca04a576d2952b53afb9
Lera Rakhmankulova <lerakhmankulova@mail.ru>
2024-03-01 11:01:12 +0300

    actions

--- machinations/convex/board.ts
+++ machinations/convex/board.ts
@@ -6,40 +6,40 @@ import { mutation, query } from "./_generated/server";
 // this is an api convex
 
 export const create = mutation({
-    args: {
-        orgId: v.string(),
-        title: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const identity = await ctx.auth.getUserIdentity();
+  args: {
+    orgId: v.string(),
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
 
-        if (!identity) {
-            throw new Error("Unauthorized");
-        }
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
 
-        const board = await ctx.db.insert("boards", {
-            title: args.title,
-            orgId: args.orgId,
-            authorId: identity.subject,
-            authorName: identity.name!,
-            imageUrl: "/placeholders/example.png",
-        });
+    const board = await ctx.db.insert("boards", {
+      title: args.title,
+      orgId: args.orgId,
+      authorId: identity.subject,
+      authorName: identity.name!,
+      imageUrl: "/placeholders/example.png",
+    });
 
-        return board;
-    },
+    return board;
+  },
 });
 
 export const remove = mutation({
-    args: { id: v.id("boards") },
-    handler: async (ctx, args) => {
-      const identity = await ctx.auth.getUserIdentity();
-  
-      if (!identity) {
-        throw new Error("Unauthorized");
-      }
-  
-      const userId = identity.subject;
-  
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const userId = identity.subject;
+
     //   const existingFavorite = await ctx.db
     //     .query("userFavorites")
     //     .withIndex("by_user_board", (q) => 
@@ -48,11 +48,38 @@ export const remove = mutation({
     //         .eq("boardId", args.id)
     //     )
     //     .unique();
-  
+
     //   if (existingFavorite) {
     //     await ctx.db.delete(existingFavorite._id);
     //   }
-  
-      await ctx.db.delete(args.id);
-    },
-  });
+
+    await ctx.db.delete(args.id);
+  },
+});
+
+export const update = mutation({
+  args: { id: v.id("boards"), title: v.string() },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const title = args.title.trim();
+
+    if (!title) {
+      throw new Error("Title is required");
+    }
+
+    if (title.length > 60) {
+      throw new Error("Title cannot be longer than 60 characters")
+    }
+
+    const board = await ctx.db.patch(args.id, {
+      title: args.title,
+    });
+
+    return board;
+  },
+});
