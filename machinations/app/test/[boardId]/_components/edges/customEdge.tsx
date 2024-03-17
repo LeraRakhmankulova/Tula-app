8bdafbc6146989644524eee407b64b13b2453294
Lera Rakhmankulova <lerakhmankulova@mail.ru>
2024-03-17 12:16:39 +0300

    Update customEdge.tsx

--- machinations/app/test/[boardId]/_components/edges/customEdge.tsx
+++ machinations/app/test/[boardId]/_components/edges/customEdge.tsx
@@ -1,33 +1,22 @@
-import React, { useCallback, useEffect, useState } from "react";
-import InputField from "./../ui/input";
+import React, { useCallback, useState } from "react";
 import {
-  BaseEdge,
   EdgeLabelRenderer,
   EdgeProps,
   StepEdge,
-  StraightEdge,
   getBezierPath,
   useReactFlow,
 } from "reactflow";
 
 export default function CustomEdge(props: EdgeProps) {
   const [inputValue, setInputValue] = useState<string>("");
-  const [focus, setFocus] = useState<boolean>(true);
 
   const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   }, []);
 
   const { setEdges } = useReactFlow();
-  const {
-    sourceX,
-    sourceY,
-    sourcePosition,
-    targetX,
-    targetY,
-    targetPosition,
-    markerEnd,
-  } = props;
+  const { sourceX, sourceY, sourcePosition, targetX, targetY, targetPosition } =
+    props;
   const [edgePath, labelX, labelY] = getBezierPath({
     sourceX,
     sourceY,
@@ -46,17 +35,20 @@ export default function CustomEdge(props: EdgeProps) {
             position: "absolute",
             transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
             fontSize: 12,
-            // everything inside EdgeLabelRenderer has no pointer events by default
-            // if you have an interactive element, set pointer-events: all
             pointerEvents: "all",
           }}
           className="nodrag nopan"
         >
           <input
-            className="w-16 h-5 bg-white py-1 px-3 text-center"
+            placeholder="value"
+            className="w-16 h-7 text-center rounded-sm"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
-            placeholder="tw"
+            style={
+              inputValue === ""
+                ? { background: "transparent" }
+                : { background: "white" }
+            }
           />
         </div>
       </EdgeLabelRenderer>
