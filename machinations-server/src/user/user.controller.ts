5c073d8c765eaadf704488cad64646138ebda0a7
Lera Rakhmankulova <lerakhmankulova@mail.ru>
2024-04-18 14:58:14 +0300

    updt

--- machinations-server/src/user/user.controller.ts
+++ machinations-server/src/user/user.controller.ts
@@ -1,34 +1,29 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CurrentUser } from 'src/decorators/user.decorator';
+import { UserDto } from './user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @Get()
+  async profile(@CurrentUser('id') id: string){
+    return this.userService.getProfile(+id)
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Post()
+  create(@Body() dto: UserDto) {
+    return this.userService.create(dto);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.userService.getById(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  update(@Param('id') id: string, @Body() dto: UserDto) {
+    return this.userService.update(+id, dto);
   }
 }
